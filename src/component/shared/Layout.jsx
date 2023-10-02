ecae5235bc693ed41d9caab38a2042dce70d3953
Anjali <anjaliprajapati1772001@gmail.com>
2023-10-03 02:33:01 +0530

    add

--- src/component/shared/Layout.jsx
+++ src/component/shared/Layout.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 export default function Layout() {
   return (
-    <div className='flex flex-row bg-neutral-100 h-screen m-screen overflow-hidden'>
-        <div>side</div>
-        {/* <div>header</div> */}
-        <div>{<Outlet/>}</div>
+    <div className="flex flex-row bg-neutral-100 h-screen m-screen overflow-hidden">
+      <div className="bg-sky-200"><Sidebar/></div>
+      <div className="p-4">
+      <div>header</div>
+      <div>{<Outlet />}</div>
+      </div>
     </div>
-  )
+  );
 }
